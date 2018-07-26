8c0405a8f80950ec6ba8c0f9af72c1265324277e
Rebecca Martin <rebecca-martin@live.com>
2018-07-26 15:52:02 -0700

    completed class demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
-console.log('App working: open app/index.js to start exploring ES6');
+// import elementToBody from './add_to_dom';
+// import $ from 'jquery';
+
+// elementToBody('h1', 'something different');
+
+// elementToBody('p', 'test test test test test test test');
+
+// $('body').append('<h1>i came from jquery</h1>');
+
+import {
+    Person,
+    SuperPerson
+} from './person';
+
+const bob = new Person('Bob');
+
+const superSara = new SuperPerson('Sara', 'pow');
+
+console.log(bob.speak('My name is Bob'));
+
+console.log(superSara.speak('My name is super Sara'));
+
+superSara.saySuperpower();
